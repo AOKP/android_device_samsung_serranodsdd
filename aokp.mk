72fa6e38da4ac93ea434268532bbfaffa415b86b
Arne Coucheron <arco68@gmail.com>
2017-02-01 01:27:15 +0100

    serranodsdd: Clean and update all copyrights
    Change-Id: I1408f8bbc10f70ff2334a7e82e6091cdfd8385b3

--- aokp.mk
+++ aokp.mk
@@ -1,13 +1,24 @@
-# Inherit some common stuff.
+# Copyright (C) 2013-2016, The CyanogenMod Project
+# Copyright (C) 2017, The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit some common AOKP stuff.
 $(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/serranodsdd/full_serranodsdd.mk)
 
-#OTA
-PRODUCT_PACKAGES += \
-    CMRemixOTA
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranodsxx TARGET_DEVICE=serranods BUILD_FINGERPRINT="samsung/serranodsxx/serranods:4.4.2/KOT49H/I9192XXUCNG2:user/release-keys" PRIVATE_BUILD_DESC="serranodsxx-user 4.4.2 KOT49H I9192XXUCNG2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranodsdd TARGET_DEVICE=serranods BUILD_FINGERPRINT="samsung/serranodsdd/serranods:4.4.2/KOT49H/I9192DDUCOF2:user/release-keys" PRIVATE_BUILD_DESC="serranodsdd-user 4.4.2 KOT49H I9192DDUCOF2 release-keys"
 
 PRODUCT_DEVICE := serranodsdd
 PRODUCT_NAME := aokp_serranodsdd
